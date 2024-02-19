16e58980d78860ea4cbf4d2858e95af486241f65
Sam <shf15@duke.edu>
2024-02-19 15:55:53 -0500

    completed some more

--- week04/q2.py
+++ week04/q2.py
@@ -1,9 +1,9 @@
-
-
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
-
+filepath = '/Users/samfuller/Desktop/545/FinTech545_sf-1/week04/DailyPrices.csv'
+prices = pd.read_csv(filepath)
 
 def return_calculate(prices, method="DISCRETE", date_column="date"):
     if date_column not in prices.columns:
@@ -39,4 +39,30 @@ def return_calculate(prices, method="DISCRETE", date_column="date"):
 # Assuming 'df' is a pandas DataFrame with price data and a 'date' column.
 # returns_df = return_calculate(df, method="DISCRETE", date_column="date")
 
-return_calculate()
+returns = (return_calculate(prices, method="LOG", date_column = "Date"))
+
+column_mean = returns['META'].mean()
+returns['META'] = returns['META'] - column_mean
+
+meta_returns = returns[['Date', 'META']]
+meta_px = prices['META'].tail(n=1).values
+#print(meta_px)
+
+# Given we hold 1 meta share we multiply today's share price by all the returns in meta_returns to give us the potentia
+meta_returns['META_px'] = (1+meta_returns['META'])*meta_px
+#print(meta_returns)
+
+
+# Normal dsitribution estimation
+sdev = meta_returns['META'].std()
+alpha = 0.95
+z_score = norm.ppf(alpha)
+
+VaR_norm = -(-z_score * sdev)*meta_px
+print("Normal VaR:", VaR_norm)
+
+# Normal distribution with EW variance
+
+
+
+
